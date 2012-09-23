f10b39ee7b7af20811e95b12c1e738c4cbfa4512
Andrew Wilkins <axwalk@gmail.com>
2012-09-23 19:11:02 +0800

    Update test case

--- llgo/testdata/switch/type.go
+++ llgo/testdata/switch/type.go
@@ -1,13 +1,19 @@
 package main
 
-func main() {
-	var x interface{}
-	x = int64(123)
-	switch x := x.(type) {
+func test(i interface{}) {
+	switch x := i.(type) {
 	case int64:
 		println("int64", x)
+	// FIXME
+	//case string:
+	//	println("string", x)
 	default:
 		println("default")
 	}
+}
+
+func main() {
+	test(int64(123))
+	test("abc")
 }
 
