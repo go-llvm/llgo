6f355c02d992c5e82332ee3757dd21c9af9c417c
Andrew Wilkins <axwalk@gmail.com>
2013-01-13 10:57:07 +0800

    llgo: Fix use of llvm.trap

--- pkg/runtime/unsafe.go
+++ pkg/runtime/unsafe.go
@@ -6,12 +6,6 @@ package runtime
 
 import "unsafe"
 
-// FIXME #llgo name: llvm.trap
-//
-// Temporarily disabled use of llvm.trap while
-// function representation changes are worked out.
-func llvm_trap() {}
-
 // #llgo name: reflect.unsafe_New
 func unsafe_New(t *rtype) unsafe.Pointer {
 	ptr := malloc(t.size)
