307b500bf10b4725bed24198a1bf7a015e300c0e
Andrew Wilkins <axwalk@gmail.com>
2013-04-29 19:56:10 +0800

    Implement cap() builtin.
    (Except for channels of course.)

--- len.go
+++ len.go
@@ -6,55 +6,61 @@ package llgo
 
 import (
 	"code.google.com/p/go.exp/go/types"
-	"fmt"
 	"github.com/axw/gollvm/llvm"
 	"go/ast"
 )
 
 func (c *compiler) VisitCap(expr *ast.CallExpr) Value {
-	// TODO implement me
-	return c.VisitLen(expr)
-}
-
-func (c *compiler) VisitLen(expr *ast.CallExpr) Value {
-	if len(expr.Args) > 1 {
-		panic("Expecting only one argument to len")
-	}
-
 	value := c.VisitExpr(expr.Args[0])
 	typ := value.Type()
 	if name, ok := underlyingType(typ).(*types.NamedType); ok {
 		typ = name.Underlying
 	}
+	var capvalue llvm.Value
+	switch typ := underlyingType(typ).(type) {
+	case *types.Pointer:
+		atyp := underlyingType(typ.Base).(*types.Array)
+		capvalue = llvm.ConstInt(c.llvmtypes.inttype, uint64(atyp.Len), false)
+	case *types.Array:
+		capvalue = llvm.ConstInt(c.llvmtypes.inttype, uint64(typ.Len), false)
+	case *types.Slice:
+		sliceval := value.LLVMValue()
+		capvalue = c.builder.CreateExtractValue(sliceval, 2, "")
+	case *types.Chan:
+		panic("cap(chan) unimplemented")
+	}
+	return c.NewValue(capvalue, types.Typ[types.Int])
+}
 
+func (c *compiler) VisitLen(expr *ast.CallExpr) Value {
+	value := c.VisitExpr(expr.Args[0])
+	typ := value.Type()
+	if name, ok := underlyingType(typ).(*types.NamedType); ok {
+		typ = name.Underlying
+	}
 	var lenvalue llvm.Value
 	switch typ := underlyingType(typ).(type) {
 	case *types.Pointer:
 		atyp := underlyingType(typ.Base).(*types.Array)
 		lenvalue = llvm.ConstInt(c.llvmtypes.inttype, uint64(atyp.Len), false)
-
 	case *types.Slice:
 		sliceval := value.LLVMValue()
 		lenvalue = c.builder.CreateExtractValue(sliceval, 1, "")
-
 	case *types.Map:
 		mapval := value.LLVMValue()
 		f := c.NamedFunction("runtime.maplen", "func f(m uintptr) int")
 		lenvalue = c.builder.CreateCall(f, []llvm.Value{mapval}, "")
-
 	case *types.Array:
 		lenvalue = llvm.ConstInt(c.llvmtypes.inttype, uint64(typ.Len), false)
-
 	case *types.Basic:
 		if isString(typ) {
 			value := value.(*LLVMValue)
 			lenvalue = c.builder.CreateExtractValue(value.LLVMValue(), 1, "")
 		}
+	case *types.Chan:
+		panic("len(chan) unimplemented")
 	}
-	if !lenvalue.IsNil() {
-		return c.NewValue(lenvalue, types.Typ[types.Int])
-	}
-	panic(fmt.Sprint("Unhandled value type: ", value.Type()))
+	return c.NewValue(lenvalue, types.Typ[types.Int])
 }
 
 // vim: set ft=go :
