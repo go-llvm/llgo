440318b60d0e8a94a67498311738562632df6935
Andrew Wilkins <axwalk@gmail.com>
2013-05-24 15:19:39 +0800

    Add dummy doc.go to overlay sync/atomic

---
+++ pkg/sync/atomic/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2013 Andrew Wilkins.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package atomic
+
+// This file exists only to appease llgo-dist.
