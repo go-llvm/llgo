645db8c33510ba845076f12ca2aa2f76a7869820
Anton Gulenko <anton.gulenko@googlemail.com>
2013-10-19 17:09:50 -0700

    Added a proxy c-file including all the gc-functionality

---
+++ pkg/runtime/gc.c
@@ -0,0 +1,6 @@
+
+// The included file contains all the functionality of the garbage collector.
+// The purpose is to have a single output binary, which greatly simplifies the build process.
+
+#include "gc/extra/gc.c"
+
