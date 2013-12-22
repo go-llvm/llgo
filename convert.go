e347197eedb8cf71134b5a06f3d0b526376ad288
Andrew Wilkins <axwalk@gmail.com>
2013-12-22 21:46:46 +0800

    Remove branching from emitted convertE2{I,V} code

--- convert.go
+++ convert.go
@@ -5,47 +5,44 @@
 package llgo
 
 import (
-	"fmt"
-
-	"code.google.com/p/go.tools/go/exact"
 	"code.google.com/p/go.tools/go/types"
 	"github.com/axw/gollvm/llvm"
 )
 
 // convertE2V converts (type asserts) an interface value to a concrete type.
 func (v *LLVMValue) convertE2V(typ types.Type) (result, success *LLVMValue) {
-	builder := v.compiler.builder
-	predicate := v.interfaceTypeEquals(typ).LLVMValue()
+	c := v.compiler
+	f := c.runtime.convertE2V.LLVMValue()
+	rtyp := c.types.ToRuntime(typ)
+	stackptr := c.stacksave()
+	ptr := c.builder.CreateAlloca(c.types.ToLLVM(typ), "")
+	args := []llvm.Value{
+		c.coerce(v.LLVMValue(), c.runtime.eface.llvm),
+		c.builder.CreatePtrToInt(rtyp, c.target.IntPtrType(), ""),
+		c.builder.CreatePtrToInt(ptr, c.target.IntPtrType(), ""),
+	}
+	succ := c.builder.CreateCall(f, args, "")
+	res := c.builder.CreateLoad(ptr, "")
+	c.stackrestore(stackptr)
+	return c.NewValue(res, typ), c.NewValue(succ, types.Typ[types.Bool])
+}
 
-	// If result is zero, then we've got a match.
-	end := llvm.InsertBasicBlock(builder.GetInsertBlock(), "end")
-	end.MoveAfter(builder.GetInsertBlock())
-	nonmatch := llvm.InsertBasicBlock(end, "nonmatch")
-	match := llvm.InsertBasicBlock(nonmatch, "match")
-	builder.CreateCondBr(predicate, match, nonmatch)
-
-	builder.SetInsertPointAtEnd(match)
-	matchResultValue := v.loadE2V(typ).LLVMValue()
-	builder.CreateBr(end)
-
-	builder.SetInsertPointAtEnd(nonmatch)
-	nonmatchResultValue := llvm.ConstNull(matchResultValue.Type())
-	builder.CreateBr(end)
-
-	builder.SetInsertPointAtEnd(end)
-	successValue := builder.CreatePHI(llvm.Int1Type(), "")
-	resultValue := builder.CreatePHI(matchResultValue.Type(), "")
-
-	successValues := []llvm.Value{llvm.ConstAllOnes(llvm.Int1Type()), llvm.ConstNull(llvm.Int1Type())}
-	successBlocks := []llvm.BasicBlock{match, nonmatch}
-	successValue.AddIncoming(successValues, successBlocks)
-	success = v.compiler.NewValue(successValue, types.Typ[types.Bool])
-
-	resultValues := []llvm.Value{matchResultValue, nonmatchResultValue}
-	resultBlocks := []llvm.BasicBlock{match, nonmatch}
-	resultValue.AddIncoming(resultValues, resultBlocks)
-	result = v.compiler.NewValue(resultValue, typ)
-	return result, success
+// mustConvertE2V calls convertE2V, panicking if the assertion failed.
+func (v *LLVMValue) mustConvertE2V(typ types.Type) *LLVMValue {
+	c := v.compiler
+	f := c.runtime.mustConvertE2V.LLVMValue()
+	rtyp := c.types.ToRuntime(typ)
+	stackptr := c.stacksave()
+	ptr := c.builder.CreateAlloca(c.types.ToLLVM(typ), "")
+	args := []llvm.Value{
+		c.coerce(v.LLVMValue(), c.runtime.eface.llvm),
+		c.builder.CreatePtrToInt(rtyp, c.target.IntPtrType(), ""),
+		c.builder.CreatePtrToInt(ptr, c.target.IntPtrType(), ""),
+	}
+	c.builder.CreateCall(f, args, "")
+	res := c.builder.CreateLoad(ptr, "")
+	c.stackrestore(stackptr)
+	return c.NewValue(res, typ)
 }
 
 // convertI2E converts a non-empty interface value to an empty interface.
@@ -62,79 +59,18 @@ func (v *LLVMValue) convertE2I(iface types.Type) (result, success *LLVMValue) {
 	f := c.runtime.convertE2I.LLVMValue()
 	typ := c.builder.CreatePtrToInt(c.types.ToRuntime(iface.Underlying()), c.target.IntPtrType(), "")
 	args := []llvm.Value{c.coerce(v.LLVMValue(), c.runtime.eface.llvm), typ}
-	res := c.coerce(c.builder.CreateCall(f, args, ""), c.types.ToLLVM(iface))
-	succ := c.builder.CreateIsNotNull(c.builder.CreateExtractValue(res, 0, ""), "")
+	res := c.builder.CreateCall(f, args, "")
+	succ := c.builder.CreateExtractValue(res, 0, "")
+	res = c.coerce(c.builder.CreateExtractValue(res, 1, ""), c.types.ToLLVM(iface))
 	return c.NewValue(res, iface), c.NewValue(succ, types.Typ[types.Bool])
 }
 
 // mustConvertE2I calls convertE2I, panicking if the assertion failed.
-func (v *LLVMValue) mustConvertE2I(typ types.Type) *LLVMValue {
-	result, ok := v.convertE2I(typ)
-	builder := v.compiler.builder
-	end := llvm.InsertBasicBlock(builder.GetInsertBlock(), "end")
-	end.MoveAfter(builder.GetInsertBlock())
-	failed := llvm.InsertBasicBlock(end, "failed")
-	builder.CreateCondBr(ok.LLVMValue(), end, failed)
-	builder.SetInsertPointAtEnd(failed)
+func (v *LLVMValue) mustConvertE2I(iface types.Type) *LLVMValue {
 	c := v.compiler
-	s := fmt.Sprintf("convertE2I(%s, %s) failed", v.typ, typ)
-	c.emitPanic(c.NewConstValue(exact.MakeString(s), types.Typ[types.String]))
-	builder.SetInsertPointAtEnd(end)
-	return result
-}
-
-// mustConvertE2V calls convertE2V, panicking if the assertion failed.
-func (v *LLVMValue) mustConvertE2V(typ types.Type) *LLVMValue {
-	result, ok := v.convertE2V(typ)
-
-	builder := v.compiler.builder
-	end := llvm.InsertBasicBlock(builder.GetInsertBlock(), "end")
-	end.MoveAfter(builder.GetInsertBlock())
-	failed := llvm.InsertBasicBlock(end, "failed")
-	builder.CreateCondBr(ok.LLVMValue(), end, failed)
-	builder.SetInsertPointAtEnd(failed)
-
-	c := v.compiler
-	s := fmt.Sprintf("convertE2V(%s, %s) failed", v.typ, typ)
-	c.emitPanic(c.NewConstValue(exact.MakeString(s), types.Typ[types.String]))
-	builder.SetInsertPointAtEnd(end)
-	return result
-}
-
-// loadE2V loads an interface value to the specified type, without
-// checking that the interface type matches.
-func (v *LLVMValue) loadE2V(typ types.Type) *LLVMValue {
-	c := v.compiler
-	if c.types.Sizeof(typ) == 0 {
-		value := llvm.ConstNull(c.types.ToLLVM(typ))
-		return c.NewValue(value, typ)
-	}
-	if c.types.Sizeof(typ) > int64(c.target.PointerSize()) {
-		ptr := c.builder.CreateExtractValue(v.LLVMValue(), 1, "")
-		ptr = c.builder.CreateBitCast(ptr, llvm.PointerType(c.types.ToLLVM(typ), 0), "")
-		return c.NewValue(c.builder.CreateLoad(ptr, ""), typ)
-	}
-	value := c.builder.CreateExtractValue(v.LLVMValue(), 1, "")
-	if _, ok := typ.Underlying().(*types.Pointer); ok {
-		value = c.builder.CreateBitCast(value, c.types.ToLLVM(typ), "")
-		return c.NewValue(value, typ)
-	}
-	bits := c.target.TypeSizeInBits(c.types.ToLLVM(typ))
-	value = c.builder.CreatePtrToInt(value, llvm.IntType(int(bits)), "")
-	value = c.coerce(value, c.types.ToLLVM(typ))
-	return c.NewValue(value, typ)
-}
-
-// interfaceTypeEquals checks, at runtime, whether an interface value
-// has the specified dynamic type.
-func (lhs *LLVMValue) interfaceTypeEquals(typ types.Type) *LLVMValue {
-	c, b := lhs.compiler, lhs.compiler.builder
-	lhsType := b.CreateExtractValue(lhs.LLVMValue(), 0, "")
-	rhsType := c.types.ToRuntime(typ)
-	f := c.runtime.eqtyp.LLVMValue()
-	t := f.Type().ElementType().ParamTypes()[0]
-	lhsType = b.CreateBitCast(lhsType, t, "")
-	rhsType = b.CreateBitCast(rhsType, t, "")
-	result := b.CreateCall(f, []llvm.Value{lhsType, rhsType}, "")
-	return c.NewValue(result, types.Typ[types.Bool])
+	f := c.runtime.mustConvertE2I.LLVMValue()
+	typ := c.builder.CreatePtrToInt(c.types.ToRuntime(iface.Underlying()), c.target.IntPtrType(), "")
+	args := []llvm.Value{c.coerce(v.LLVMValue(), c.runtime.eface.llvm), typ}
+	res := c.coerce(c.builder.CreateCall(f, args, ""), c.types.ToLLVM(iface))
+	return c.NewValue(res, iface)
 }
--- pkg/runtime/interfaces.go
+++ pkg/runtime/interfaces.go
@@ -46,13 +46,19 @@ func convertI2E(i iface) eface {
 
 const ptrsize = unsafe.Sizeof(uintptr(0))
 
-func convertE2I(e eface, typ unsafe.Pointer) (result iface) {
-	if e.rtyp == nil || e.rtyp.uncommonType == nil {
-		return iface{}
+func convertE2I(e eface, typ unsafe.Pointer) (ok bool, result iface) {
+	if e.rtyp == nil {
+		// nil conversion
+		return true, iface{}
+	}
+	if e.rtyp.uncommonType == nil {
+		// unnamed type, cannot succeed
+		return false, iface{}
 	}
 	targetType := (*interfaceType)(unsafe.Pointer(typ))
 	if len(targetType.methods) > len(e.rtyp.methods) {
-		return iface{}
+		// too few methods
+		return false, iface{}
 	}
 	size := unsafe.Sizeof(*result.tab)
 	size += (uintptr(len(targetType.methods)) - 1) * ptrsize
@@ -72,13 +78,26 @@ func convertE2I(e eface, typ unsafe.Pointer) (result iface) {
 		}
 		if !found {
 			free(unsafe.Pointer(tab))
-			return iface{}
+			return false, iface{}
 		}
 	}
 	result.tab = tab
 	result.data = e.data
 	result.tab.inter = targetType
 	result.tab.typ = e.rtyp
+	return true, result
+}
+
+func mustConvertE2I(e eface, typ unsafe.Pointer) (result iface) {
+	ok, result := convertE2I(e, typ)
+	if !ok {
+		estring := "nil"
+		if e.rtyp != nil {
+			estring = *e.rtyp.string
+		}
+		ifaceType := (*interfaceType)(typ)
+		panic("interface conversion: interface is " + estring + ", not " + *ifaceType.string)
+	}
 	return result
 }
 
@@ -87,4 +106,34 @@ func reflect_ifaceE2I(t *rtype, src interface{}, dst unsafe.Pointer) {
 	// TODO
 	println("TODO: reflect.ifaceE2I")
 	llvm_trap()
+}
+
+func convertE2V(e eface, typ_, ptr unsafe.Pointer) bool {
+	typ := (*rtype)(typ_)
+	if !eqtyp(e.rtyp, typ) {
+		return false
+	}
+	if typ.size == 0 {
+		return true
+	}
+	var data unsafe.Pointer
+	if typ.size <= unsafe.Sizeof(data) {
+		data = unsafe.Pointer(&e.data)
+	} else {
+		data = unsafe.Pointer(e.data)
+	}
+	// TODO(axw) use copy alg
+	memcpy(ptr, data, typ.size)
+	return true
+}
+
+func mustConvertE2V(e eface, typ_, ptr unsafe.Pointer) {
+	if !convertE2V(e, typ_, ptr) {
+		estring := "nil"
+		if e.rtyp != nil {
+			estring = *e.rtyp.string
+		}
+		typ := (*rtype)(typ_)
+		panic("interface conversion: interface is " + estring + ", not " + *typ.string)
+	}
 }
